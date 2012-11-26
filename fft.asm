bc8b4127e96afc0ca424dfd134e6b76ad9ee6b54
andykoz <adkozlov@gmail.com>
2012-11-26 23:59:32 +0400

    roots

--- fft.asm
+++ fft.asm
@@ -1,3 +1,9 @@
+section .const
+
+pi dq 3.14159265
+
+section .text
+
 ; parameters
 %define in_data dword [ebp + 8]
 %define size dword [ebp + 12]
@@ -10,6 +16,26 @@ fft:
 	push esi
 	push edi
 	
+	; double *roots = (double*) calloc(2 * size, sizeof(double))
+	roots dq 2 * size dup (0)
+	
+	; double alpha = 2 * M_PI / size
+	movsd xmm0, pi
+	mulsd xmm0, 2
+	divsd xmm0, size
+	
+	; for (i = 0; i < size; ++i)
+	xor ecx, ecx 
+	roots_1:
+		cmp ecx, size
+		jae roots_2
+			
+		movsd xmm1, ecx
+		
+		inc ecx
+		jmp roots_1
+	
+	roots_2:	
 	
 	
 	pop edi ;save regs
