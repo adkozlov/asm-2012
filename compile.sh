cc110cb472348b152abfbd2aeae4ea52364b3e4d
andykoz <adkozlov@gmail.com>
2012-11-27 03:06:11 +0400

    refactor

--- compile.sh
+++ compile.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-yasm fft.asm -f elf32
+yasm ./src/fft.asm -o ./bin/fft.o -f elf32
+echo "asm compiling success"
+
+gcc -c ./src/main.c -o ./bin/main.o
+echo "c compiling success"
+
+ld -o ./bin/fft --dynamic-linker /lib/ld-linux.so.2 -melf_i386 -lc ./bin/main.o ./bin/fft.o
+echo "linking success"
+
+./bin/fft
+echo "run sucess"
