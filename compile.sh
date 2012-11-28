a535c1a7219afbfc48fff9a3e24a0c1347f07fcc
andykoz <adkozlov@gmail.com>
2012-11-28 16:14:12 +0400

    segment fault

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yasm ./src/fft.asm -f elf32 -o ./bin/fft.o
-gcc -c ./src/main.c -o ./bin/main.o -m32
+yasm ./src/fft.asm -f elf32 -o ./bin/fft.o -g dwarf2
+gcc -c ./src/main.c -o ./bin/main.o -m32 -g
 
 gcc ./bin/main.o ./bin/fft.o -m32 -o fft
--- src/fft.asm
+++ src/fft.asm
@@ -24,7 +24,7 @@ global fft
 fft:
 	push ebp
 	mov ebp, esp
-	sub esp, 32 ; todo: local vars
+	sub esp, 36 ; todo: local vars
 	push ebx
 	push esi
 	push edi	
@@ -38,14 +38,14 @@ fft:
 		inc ecx
 		cmp eax, [ebp + 12]
 		jb log_loop
-	mov dword [ebp - 16], ecx
+	mov dword [ebp - 4], ecx
 
 	; int *rev = (int*) calloc(size, sizeof(int))
 	call calloc_int_size
-	mov [ebp - 20], eax
+	mov [ebp - 8], eax
 	
-	mov dword [ebp - 20], 0 ; rev[0] = 0
-	mov dword [ebp - 24], -1 ; int high1 = -1
+	mov dword [ebp - 12], 0 ; rev[0] = 0
+	mov dword [ebp - 16], -1 ; int high1 = -1
 	
 	xor ecx, ecx
 	rev_loop: ; for (i = 1; i < size; ++i)
@@ -56,7 +56,7 @@ fft:
 		dec eax
 		test eax, ecx
 		jnz .false
-			inc dword [ebp - 24]
+			inc dword [ebp - 16]
 			jmp .endif
 		.false:
 		.endif:
@@ -64,18 +64,18 @@ fft:
 		; i ^ (1 << high1)
 		xor eax, eax
 		inc eax
-		mov edx, dword [ebp - 24]
+		mov edx, dword [ebp - 16]
 		push ecx
 		mov ecx, edx
 		shl eax, cl
 		pop ecx
 		xor eax, ecx
 		
-		push [ebp + 4 * eax - 20]
+		push dword [ebp + 4 * eax - 8]
 		
 		; 1 << (k - high1 - 1)
-		mov edx, dword [ebp - 16]
-		sub edx, dword [ebp - 24]
+		mov edx, dword [ebp - 4]
+		sub edx, dword [ebp - 16]
 		dec edx
 		xor eax, eax
 		inc eax
@@ -87,14 +87,14 @@ fft:
 		
 		or eax, esp
 		add esp, 4
-		mov [esp + 4 * ecx - 20] , eax
+		mov [esp + 4 * ecx - 12] , eax
 		
 		cmp ecx, [ebp + 12]
 		jb rev_loop
 
 	; double *roots = (double*) calloc(2 * size, sizeof(double))
 	call calloc_double_2size
-	mov [ebp - 4], eax
+	mov [ebp - 20], eax
 
 	; double alpha = 2 * M_PI / size
 	fldpi
@@ -105,33 +105,40 @@ fft:
 
 	; for (i = 0; i < size; ++i)
 	mov ecx, [ebp + 12]
-	mov [ebp - 8], ecx
 	roots_loop:
-		dec	dword [ebp - 8]
+		dec	ecx
 
 		fld st0
-		fimul dword [ebp - 8]
-		fsincos
+		push ecx
+		fimul dword [esp]
+		pop ecx
+		fsincos		
+		
+		mov eax, ecx
+		shl eax, 1
+		fstp qword [ebp + 8 * eax - 20]
+		inc eax
+		fstp qword [ebp + 8 * eax - 20]
 
-		cmp dword [ebp - 8], 0
+		cmp ecx, 0
 		jnz roots_loop
 
 	; double *cur = (double*) calloc(2 * size, sizeof(double));
 	call calloc_double_2size
-	mov [ebp - 12], eax
+	mov [ebp - 24], eax
 	
 	; free(rev)
-	push dword [ebp - 20]
+	push dword [ebp - 8]
 	call free
 	add esp, 4
 
 	; free(roots)
-	push dword [ebp - 4]
+	push dword [ebp - 20]
 	call free
 	add esp, 4
 
 	; return cur;
-	mov eax, [ebp - 12]
+	mov eax, [ebp - 24]
 
 	pop edi
 	pop esi
